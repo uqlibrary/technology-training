83a50a30a83e373c837213c33c5c0a1c1056e1c6
camwest5 <118239146+camwest5@users.noreply.github.com>
2025-05-21 11:16:55 +1000

    Fibonnaci version, original idea
    Replace looping files with fibonacci
    
    Expand fib - will probably delete

--- Python/archive/revamp/5-python_toolkit/simple_io.py
+++ Python/archive/revamp/5-python_toolkit/simple_io.py
@@ -1,12 +1,17 @@
 def write(path, contents, mode = "a"):
+    """Writes 'contents' to the file at 'path'. 'mode' is passed to open()"""
     with open(path, mode) as file:
         file.write(contents)
 
-def read(path):
-    with open(path) as file:
-        return file.read()
+def fib_i(i):
+    """Calculates the ith element of the Fibonacci sequence."""
+    Fibonacci = [1,1]
 
+    while len(Fibonacci) < i:
+        # Calculate next digit in sequence
+        n = Fibonacci[-1] + Fibonacci[-2]
 
-a = 1
-print(a)
+        # Append to list
+        Fibonacci.append(n)
 
+    return Fibonacci[-1]
