f9555e40c5ec83dd6f421d7189df467a3c5da8da
camwest5 <118239146+camwest5@users.noreply.github.com>
2025-10-09 16:01:19 +1000

    Clean and postprocess upcoming link automation. Also freezes.
    - Postprocessing script means that all changes happen during render and are reversed: injected messages should never be reflected in .qmd files.
    - Scripts cleaned and commented for readability.

--- _preprocessing/preprocessing.py
+++ _preprocessing/preprocessing.py
@@ -1,8 +1,8 @@
 import check_links
 import update_live_links
 
-print("\nChecking for broken links")
-check_links.check_links()
+# print("\nChecking for broken links")
+# check_links.check_links()
 
 print("\nUpdating live links")
 update_live_links.update_live_links()
--- _preprocessing/update_live_links.py
+++ _preprocessing/update_live_links.py
@@ -2,9 +2,13 @@ import os
 import requests
 from datetime import datetime, timezone
 import textwrap
+import yaml
+
+MESSAGE_HEADER = f"<!-- Upcoming workshops -->"
 
 
 def update_live_links() -> None:
+    """Loops through active .qmds looking for matches with upcoming StudentHub events. On match hit, injects an upcoming event message with links."""
     # Call StudentHub api, get sessions
     r = requests.get(
         "https://api.library.uq.edu.au/v1/training_events?take=100&filterIds[]=104&ts=1745812216416",
@@ -13,114 +17,90 @@ def update_live_links() -> None:
         },
     )
 
-    r.raise_for_status()
-
     sh_body = r.json()
 
-    with open("_quarto.yml") as f:
-        project_file = f.read()
+    # Loop through active QMDs and update
+    for qmd_path in list_active_qmds():
 
-    # Go through pages on site
-    for path, dirs, files in os.walk(".\\"):
-        if len(path) > 2 and (path[2] == "_" or path[2] == "."):
-            continue
+        with open(qmd_path, encoding="utf8") as f:
+            content: str = f.read()
 
-        # Only consider files that are getting published (i.e., in _quarto.yml)
+        # Get title
+        title_start = content.find("title") + 6
+        title = content[title_start : content.find("\n", title_start)]
+        clean_title = clean(title)
 
-        # Currently DOES NOT WORK FOR IPYNB
-        content_filepaths = [
-            os.path.join(path, file)
-            for file in files
-            if (file in project_file)
-            and (file[-2:] == "md")  # or file[-6:] == ".ipynb")
-        ]
+        # Get earliest upcoming event
+        date = None
+        upcoming_event: dict = {}
 
-        for qmd_path in content_filepaths:
-            # Get title
-            with open(qmd_path, encoding="utf8") as f:
-                content = f.read()
+        for event in sh_body:
+            new_date = datetime.strptime(event["start"], "%Y-%m-%dT%H:%M:%S%z")
+            if clean_title in clean(event["name"]) and (
+                date is None or new_date < date
+            ):
+                upcoming_event = event
 
-            title_start = content.find("title") + 6
-            title = content[title_start : content.find("\n", title_start)]
+        if len(upcoming_event) == 0:
+            break
 
-            clean_title = clean(title)
+        # Form message
+        link = f"https://studenthub.uq.edu.au/students/events/detail/{upcoming_event["entityId"]}"
+        booking_message = "[Book in to the next offering now.]"
+        formatted_date = datetime.strptime(
+            upcoming_event["start"], "%Y-%m-%dT%H:%M:%S%z"
+        ).strftime("%a %b %d at %I:%M %p.")
 
-            date = None
-            upcoming_event: dict = {}
+        message = textwrap.dedent(
+            f"""
+        {MESSAGE_HEADER}
+        :::{{.callout-tip}}
+        # Upcoming workshop(s) available!
 
-            for event in sh_body:
-                new_date = datetime.strptime(event["start"], "%Y-%m-%dT%H:%M:%S%z")
-                if clean_title in clean(event["name"]) and (
-                    date is None or new_date < date
-                ):
-                    upcoming_event = event
+        The next workshop is on **{formatted_date}**
+        
+        {booking_message}({link})
 
-            if len(upcoming_event) > 0:
-                # upcoming_event stores everything
-                link = f"https://studenthub.uq.edu.au/students/events/detail/{upcoming_event["entityId"]}"
-                booking_message = "[Book in to the next offering now.]"
-                formatted_date = datetime.strptime(
-                    upcoming_event["start"], "%Y-%m-%dT%H:%M:%S%z"
-                ).strftime("%a %b %d at %I:%M %p.")
+        Alternatively, [check our calendar](https://web.library.uq.edu.au/study-and-learning-support/training-and-workshops/online-and-person-workshops#keyword=;campus=;weekstart=) for future events.
+        :::
+        """
+        )
 
-                message = f"""
-                <!-- Do not touch this - it will automatically update during render -->
-                :::{{.callout-tip}}
-                # Upcoming workshop(s) available!
+        # Insert message after YAML
+        yaml_end = content.find("---", content.find("---") + 4) + 5
+        new_content = "".join([content[:yaml_end], message, content[yaml_end:]])
 
-                The next workshop is on **{formatted_date}**
-                
-                {booking_message}({link})
+        with open(qmd_path, "w", encoding="utf8") as f:
+            f.write(new_content)
 
-                Alternatively, [check our calendar](https://web.library.uq.edu.au/study-and-learning-support/training-and-workshops/online-and-person-workshops#keyword=;campus=;weekstart=) for future events.
-                :::
-                """
-                message = textwrap.dedent(message)
+    return None
 
-                # Check if existing message present
 
-                if message[:100] in content:
-                    # Replace link
-                    line_start = content.find(booking_message)
+def remove_upcoming_links() -> None:
+    """Loops through active .qmds and removes the upcoming links message. Directly reverses changes made by update_live_links()."""
+    for qmd_path in list_active_qmds():
 
-                    link_start = content.find(
-                        "https://studenthub.uq.edu.au/students/events/detail/",
-                        line_start,
-                    )
+        with open(qmd_path, encoding="utf8") as f:
+            content: str = f.read()
 
-                    line_end = content.find("\n", link_start)
+        if MESSAGE_HEADER not in content:
+            break
 
-                    new_content = "".join(
-                        [content[:link_start], link, ")", content[line_end:]]
-                    )
-                else:
-                    i_start = content.find("---", content.find("---") + 5) + 5
+        message_start = content.find(MESSAGE_HEADER)
+        message_end = content.find(":::", content.find(":::", message_start) + 4) + 5
 
-                    new_content = "\n".join(
-                        [content[:i_start], message, content[i_start:]]
-                    )
-            else:
-                message_start = content.find(
-                    "<!-- Do not touch this - it will automatically update during render -->"
-                )
+        old_message = content[message_start:message_end]
 
-                if message_start == -1:
-                    new_content = content
+        new_content = content.replace(old_message, "")
 
-                else:
-                    message_end = content.find(":::", content.find(":::") + 3) + 4
-
-                    new_content = "".join(
-                        [content[:message_start], content[message_end:]]
-                    )
-
-            with open(qmd_path, "w", encoding="utf8") as f:
-                f.write(new_content)
+        with open(qmd_path, "w", encoding="utf8") as f:
+            f.write(new_content)
 
     return None
 
 
 def clean(s: str) -> str:
+    """Returns a lowercase copy of s stripped of all whitespace and punctuation."""
     s = s.strip().lower().replace(r"\n", "")
     new_s = list(s)
     for i, char in enumerate(s):
@@ -128,6 +108,28 @@ def clean(s: str) -> str:
             new_s[i] = ""
 
     return "".join(new_s)
+
+
+def list_active_qmds() -> list[str]:
+    """Walks through current dir and returns a list of qmds that are also in _quarto.yml"""
+    with open("_quarto.yml") as f:
+        project_file = f.read()
+
+    content_filepaths = []
+    # Go through pages on site
+    for path, dirs, files in os.walk(".\\"):
+        if len(path) > 2 and (path[2] == "_" or path[2] == "."):
+            continue
+
+        # Only consider files that are getting published (i.e., in _quarto.yml)
+        content_filepaths = [
+            os.path.join(path, file)
+            for file in files
+            if (file in project_file)
+            and (file[-2:] == "md")  # or file[-6:] == ".ipynb")
+        ]
+
+    return content_filepaths
 
 
 if __name__ == "__main__":
