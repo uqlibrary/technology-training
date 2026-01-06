743e408166a3c720adfbaab2569e7888c44f56eb
Stéphane Guillou <stephane.guillou@member.fsf.org>
2026-01-06 15:46:44 +1000

    R: manually record dependency for renv
    the _dependencies.R files is one way to record
    extra package dependencies not automatically
    detected by renv.
    The package 'see' is needed for generating
    diagnostic plots in the ANOVA/lm session.
    See: https://rstudio.github.io/renv/reference/dependencies.html#missing-dependencies

---
+++ _dependencies.R
@@ -0,0 +1,6 @@
+# This file is one solution to the issue of renv not capturing
+# some package dependencies.
+# See https://rstudio.github.io/renv/reference/dependencies.html
+
+# needed for the ANOVA/lm session, to generate diagnostic plots
+library("see")
