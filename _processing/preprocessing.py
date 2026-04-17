fe978f1af2f4abe0b340ebb9111f9ab23ba8d351
Cameron West <cameron.west@uq.edu.au>
2026-04-17 12:56:14 +1000

    Remove "check links" from local processing

--- _processing/preprocessing.py
+++ _processing/preprocessing.py
@@ -6,9 +6,9 @@ if os.getenv("IN_GITHUB_WORKFLOW") == "true":
     # Insert banners for upcoming events
     print("\nUpdating live links")
     update_live_links.update_live_links()
-else:
-    # Check for broken links and update logs
-    print("\nChecking for broken links")
-    check_links.check_links(verbose=True)
+# else:
+#     # Check for broken links and update logs
+#     print("\nChecking for broken links")
+#     check_links.check_links(verbose=True)
 
 print("Preprocessing complete.")
